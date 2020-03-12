e055475d66d39edc478cd51da739a4d0a3e73d04
Capablanca4 <44622746+Capablanca4@users.noreply.github.com>
2020-03-12 11:35:25 +0100

    Update RondPoint.c
    Ajout des include dans le code

--- RondPoint.c
+++ RondPoint.c
@@ -1,6 +1,13 @@
-
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/stat.h>
 
 int main()
 {
-return 0;
+  
+  return 0;
 }
