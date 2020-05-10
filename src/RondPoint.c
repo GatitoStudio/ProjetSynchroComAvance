18ed221dd229c89b710505cb2da4ca016a1f9b19
Capablanca4 <44622746+Capablanca4@users.noreply.github.com>
2020-05-10 13:28:21 +0200

    Ajout des premiers éléments de logique
    Ajout des fonctions insertionVoiture, suppressionVoiture et avanceVoiture gérant le déplacement des voitures contenu dans le tableau des voitures

--- src/RondPoint.c
+++ src/RondPoint.c
@@ -6,8 +6,34 @@
 #include <sys/msg.h>
 #include <sys/stat.h>
 
+int voitures[4] = {0,1,2,3};
+
 int main()
 {
-  
+  //Insérer ici la logique du programme
   return 0;
+}
+
+int DeplaceVoiture(){
+  //Insérer ici la logique pour déplacer les voitures
+  return 0;
+}
+
+void avanceVoiture(){
+  int voitureN = voiture[0];
+  int voitureO = voiture[1];
+  int voitureS = voiture[2];
+  int voitureE = voiture[3];
+  voiture[0] = voitureE;
+  voiture[1] = voitureN;
+  voiture[2] = voitureO;
+  voiture[3] = voitureS;
+}
+
+void suppressionVoiture(int indice){
+  voitures[indice] = null;
+}
+
+void insertionVoiture(int indice,int voiture){
+  voitures[indice] = voiture;
 }
